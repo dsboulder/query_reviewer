9af2449439dbfc885a4c05ec320142c0143a6fc2
Kevin Hall <khall@spongefish.com>
2008-04-09 21:59:09 +0000

    kh: Adding key length warning for queries

--- lib/query_reviewer/mysql_analyzer.rb
+++ lib/query_reviewer/mysql_analyzer.rb
@@ -4,9 +4,10 @@ module QueryReviewer
       analyze_select_type!
       analyze_query_type!
       analyze_key!
-      analyze_extras!      
+      analyze_extras!
+      analyze_keylen!
     end
-    
+
     def analyze_select_type!
       if select_type.match /uncacheable subquery/
         warn(:severity => 10, :field => "select_type", :desc => "Subquery must be run once for EVERY row in main query")
@@ -14,7 +15,7 @@ module QueryReviewer
         warn(:severity => 2, :field => "select_type", :desc => "Dependent subqueries can not be executed while the main query is running")
       end
     end
-    
+
     def analyze_query_type!
       case query_type
       when "system", "const", "eq_ref":
@@ -29,20 +30,20 @@ module QueryReviewer
         warn(:severity => 9, :field => "query_type", :desc => "Full table scan") unless !extra.include?("using where")
       end
     end
-    
+
     def analyze_key!
       if self.key == "const"
         praise "Way to go!"
-      elsif self.key.blank? && !self.rows.blank?
+      elsif self.key.blank? && !self.extra.include?("select tables optimized away")
         warn :severity => 6, :field => "key", :desc => "No index was used here. In this case, that meant scanning #{self.rows} rows."
       end
     end
-    
+
     def analyze_extras!
       if self.extra.match(/range checked for each record/)
         warn :severity => 4, :problem => "Range checked for each record", :desc => "MySQL found no good index to use, but found that some of indexes might be used after column values from preceding tables are known"
       end
-        
+
       if self.extra.match(/using filesort/)
         warn :severity => 2, :problem => "Using filesort", :desc => "MySQL must do an extra pass to find out how to retrieve the rows in sorted order."
       end
@@ -50,6 +51,12 @@ module QueryReviewer
       if self.extra.match(/using temporary/)
         warn :severity => 10, :problem => "Using temporary table", :desc => "To resolve the query, MySQL needs to create a temporary table to hold the result."
       end
-    end 
+    end
+
+    def analyze_keylen!
+      if !self.key_len.to_i.nil? && (self.key_len.to_i > QueryReviewer::CONFIGURATION["warn_key_length"])
+        warn :severity => (key_len.to_i > QueryReviewer::CONFIGURATION["critical_key_length"] ? 8 : 6), :problem => "Long key length (#{self.key_len.to_i})", :desc => "The key used for the index was rather long, potentially affecting indices in memory"
+      end
+    end
   end
 end
