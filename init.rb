66d6552ecd5e2ba86874621cd02d8ab7f412058c
David Stevenson <david@flouri.sh>
2008-06-25 03:50:19 +0000

    Rails 2.1 fix... as per suggestion from nice people on the interweb

--- init.rb
+++ init.rb
@@ -4,3 +4,4 @@ require 'query_reviewer'
 ActiveRecord::ConnectionAdapters::MysqlAdapter.send(:include, QueryReviewer::MysqlAdapterExtensions)
 ActionController::Base.send(:include, QueryReviewer::ControllerExtensions)
 Array.send(:include, QueryReviewer::ArrayExtensions)
+ActionController::Base.append_view_path(File.dirname(__FILE__) + "/lib/query_reviewer/views") if ActionController::Base.respond_to?(:append_view_path)
